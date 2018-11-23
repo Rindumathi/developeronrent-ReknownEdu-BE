91d41c5412b8551181981aa2bf813ffd22bea8f0
Rindumathi <induece94@gmail.com>
2018-11-23 17:48:06 +0530

    update master prb

--- routes/index.js
+++ routes/index.js
@@ -156,10 +156,14 @@ router.put('/probabilitytool', (req, res, next) => {
         //if (!req.body.pg_10th || !req.body.pg_12th || !req.body.pg_gpa || !req.body.pg_Backlogs || !req.body.pg_gre || !req.body.pg_gmat || !req.body.pg_sat || !req.body.pg_toefl || !req.body.pg_ielts || !req.body.pg_pte || !req.body.pg_wrkexp || !req.body.pg_internship || !req.body.pg_projects || !req.body.pg_workshops || !req.body.pg_extraskills || !req.body.pg_sports || !req.body.pg_conference || !req.body.pg_paperpresentation || !req.body.pg_patents || !req.body.pg_seminars || !req.body.pg_recommendation || !req.body.pg_universityreputation) {
         //   res.json({ success: false, message: 'please fill all requirements' });
         //} else {
+        var x = 10;
+        var pg_10th_up =''+req.body.pg_10th*x+'';
+        var pg_12th_up =''+req.body.pg_12th*x+'';
+        var pg_gpa_up =''+req.body.pg_gpa*x+'';
         var query = {
-            pg_10th: req.body.pg_10th,
-            pg_12th: req.body.pg_12th,
-            pg_gpa: req.body.pg_gpa,
+            pg_10th: pg_10th_up,
+            pg_12th: pg_12th_up,
+            pg_gpa: pg_gpa_up,
             pg_Backlogs: req.body.pg_Backlogs,
             pg_gre: req.body.pg_gre,
             pg_gmat: req.body.pg_gmat,
@@ -180,7 +184,8 @@ router.put('/probabilitytool', (req, res, next) => {
             pg_recommendation: req.body.pg_recommendation,
             pg_universityreputation: req.body.pg_universityreputation
         };
-        if ((req.body.pg_gre >= 330 && req.body.pg_gre <= 340) && (req.body.pg_gmat >= 740 && req.body.pg_gmat <= 800) && (req.body.pg_sat >= 1500 && req.body.pg_sat <= 1600) && (req.body.pg_toefl >= 115 && req.body.pg_toefl <= 120) && (req.body.pg_ielts >= 8 && req.body.pg_ielts <= 9) && (req.body.pg_pte >= 80 && req.body.pg_pte <= 90) && req.body.pg_wrkexp == "6 Years" && req.body.pg_internship == "5 Certificates" && req.body.pg_projects == 6 && (req.body.pg_workshops == 3 || req.body.pg_workshops == 4) && req.body.pg_extraskills >= 15 && req.body.pg_sports >= 5 && req.body.pg_conference >= 5 && req.body.pg_paperpresentation >= 5 && req.body.pg_recommendation == "Exemplary" && req.body.pg_universityreputation == "Exemplary" && req.body.pg_patents >= 5 && req.body.pg_seminars >= 5 && req.body.pg_Backlogs == 0 && (req.body.pg_12th >= 9.5 && req.body.pg_12th <= 10) && (req.body.pg_10th >= 9.5 && req.body.pg_10th <= 10) && (req.body.pg_gpa >= 9.5 && req.body.pg_gpa <= 10)) {
+        if ((req.body.pg_gre >= 330 && req.body.pg_gre <= 340) && (req.body.pg_gmat >= 740 && req.body.pg_gmat <= 800) && (req.body.pg_sat >= 1500 && req.body.pg_sat <= 1600) && (req.body.pg_toefl >= 115 && req.body.pg_toefl <= 120) && (req.body.pg_ielts >= 8.0 && req.body.pg_ielts <= 9.0) && (req.body.pg_pte >= 80 && req.body.pg_pte <= 90) && req.body.pg_wrkexp == "6 Years" && req.body.pg_internship == "5 Certificates" && req.body.pg_projects == 6 && (req.body.pg_workshops == 3 || req.body.pg_workshops == 4) && req.body.pg_extraskills >= 15 && req.body.pg_sports >= 5 && req.body.pg_conference >= 5 && req.body.pg_paperpresentation >= 5 && req.body.pg_recommendation == "Exemplary" && req.body.pg_universityreputation == "Exemplary" && req.body.pg_patents >= 5 && req.body.pg_seminars >= 5 && req.body.pg_Backlogs == 0 && (pg_12th_up >= 95 && pg_12th_up <= 100) && (pg_10th_up >= 95 && pg_10th_up <= 100) && (pg_gpa_up >= 95 && pg_gpa_up <= 100)) {
+            console.log(query);
             dbo.collection("universitydatas").find(query).toArray(function (err, titanium) {
                 if (err) throw err;
                 //console.log(result);
@@ -189,7 +194,8 @@ router.put('/probabilitytool', (req, res, next) => {
                 // db.close();
             });
         } else {
-            if ((req.body.pg_gre >= 325 && req.body.pg_gre <= 329) && (req.body.pg_gmat >= 680 && req.body.pg_gmat <= 739) && (req.body.pg_sat >= 1400 && req.body.pg_sat <= 1499) && (req.body.pg_toefl >= 110 && req.body.pg_toefl <= 114) && (req.body.pg_ielts >= 7.5 && req.body.pg_ielts <= 9) && (req.body.pg_pte >= 75 && req.body.pg_pte <= 79) && req.body.pg_wrkexp == "5 Years" && req.body.pg_internship == "4 Certificates" && req.body.pg_projects == 5 && (req.body.pg_workshops == 3 || req.body.pg_workshops == 4) && req.body.pg_sports >= 4 && req.body.pg_conference >= 4 && req.body.pg_paperpresentation >= 4 && req.body.pg_recommendation == "Amazing" && req.body.pg_universityreputation == "Amazing" && req.body.pg_patents >= 4 && req.body.pg_seminars >= 4 && (req.body.pg_12th >= 9 && req.body.pg_12th <= 9.4) && (req.body.pg_10th >= 9 && req.body.pg_10th <= 9.4) && (req.body.pg_gpa >= 8.5 && req.body.pg_gpa <= 9.4) && (req.body.pg_extraskills >= 12 && req.body.pg_extraskills <= 14) && (req.body.pg_Backlogs >= 1 && req.body.pg_Backlogs <= 3)) {
+            if ((req.body.pg_gre >= 325 && req.body.pg_gre <= 329) && (req.body.pg_gmat >= 680 && req.body.pg_gmat <= 739) && (req.body.pg_sat >= 1400 && req.body.pg_sat <= 1499) && (req.body.pg_toefl >= 110 && req.body.pg_toefl <= 114) && (req.body.pg_ielts >= 7.5 && req.body.pg_ielts <= 9.0) && (req.body.pg_pte >= 75 && req.body.pg_pte <= 79) && req.body.pg_wrkexp == "5 Years" && req.body.pg_internship == "4 Certificates" && req.body.pg_projects == 5 && (req.body.pg_workshops == 3 || req.body.pg_workshops == 4) && req.body.pg_sports >= 4 && req.body.pg_conference >= 4 && req.body.pg_paperpresentation >= 4 && req.body.pg_recommendation == "Amazing" && req.body.pg_universityreputation == "Amazing" && req.body.pg_patents >= 4 && req.body.pg_seminars >= 4 && (pg_12th_up >= 90 && pg_12th_up <= 94) && (pg_10th_up >= 90 && pg_10th_up <= 94) && (pg_gpa_up >= 85 && pg_gpa_up <= 94) && (req.body.pg_extraskills >= 12 && req.body.pg_extraskills <= 14) && (req.body.pg_Backlogs >= 1 && req.body.pg_Backlogs <= 3)) {
+                
                 dbo.collection("universitydatas").find(query).toArray(function (err, platinum) {
                     if (err) throw err;
                     //console.log(result);
@@ -198,7 +204,8 @@ router.put('/probabilitytool', (req, res, next) => {
                     // db.close();
                 });
             } else {
-                if (req.body.pg_seminars >= 3 && req.body.pg_recommendation == "Superb" && req.body.pg_universityreputation == "Superb" && req.body.pg_patents >= 3 && req.body.pg_paperpresentation >= 3 && req.body.pg_conference >= 3 && req.body.pg_sports >= 3 && (req.body.pg_extraskills >= 10 && req.body.pg_extraskills <= 11) && (req.body.pg_workshops == 3 || req.body.pg_workshops == 4) && req.body.pg_projects == 4 && req.body.pg_internship == "3 Certificates" && req.body.pg_wrkexp == "4 Years" && (req.body.pg_gre >= 315 && req.body.pg_gre <= 324) && (req.body.pg_gmat >= 650 && req.body.pg_gmat <= 679) && (req.body.pg_sat >= 1300 && req.body.pg_sat <= 1399) && (req.body.pg_toefl >= 100 && req.body.pg_toefl <= 109) && (req.body.pg_ielts >= 7 && req.body.pg_ielts <= 9) && (req.body.pg_pte >= 70 && req.body.pg_pte <= 74) && (req.body.pg_Backlogs >= 4 && req.body.pg_Backlogs <= 6) && (req.body.pg_gpa >= 8 && req.body.pg_gpa <= 8.4) && (req.body.pg_12th >= 8.5 && req.body.pg_12th <= 8.9) && (req.body.pg_10th >= 8.5 && req.body.pg_10th <= 8.9)) {
+                //console.log(query);
+                if (req.body.pg_seminars >= 3 && req.body.pg_recommendation == "Superb" && req.body.pg_universityreputation == "Superb" && req.body.pg_patents >= 3 && req.body.pg_paperpresentation >= 3 && req.body.pg_conference >= 3 && req.body.pg_sports >= 3 && (req.body.pg_extraskills >= 10 && req.body.pg_extraskills <= 11) && (req.body.pg_workshops == 3 || req.body.pg_workshops == 4) && req.body.pg_projects == 4 && req.body.pg_internship == "3 Certificates" && req.body.pg_wrkexp == "4 Years" && (req.body.pg_gre >= 315 && req.body.pg_gre <= 324) && (req.body.pg_gmat >= 650 && req.body.pg_gmat <= 679) && (req.body.pg_sat >= 1300 && req.body.pg_sat <= 1399) && (req.body.pg_toefl >= 100 && req.body.pg_toefl <= 109) && (req.body.pg_ielts >= 7.0 && req.body.pg_ielts <= 9.0) && (req.body.pg_pte >= 70 && req.body.pg_pte <= 74) && (req.body.pg_Backlogs >= 4 && req.body.pg_Backlogs <= 6) && (pg_gpa_up >= 80 && pg_gpa_up <= 84) && (pg_12th_up >= 85 && pg_12th_up <= 89) && (pg_10th_up >= 85 && pg_10th_up <= 89)) {
                     dbo.collection("universitydatas").find(query).toArray(function (err, diamond) {
                         if (err) throw err;
                         //console.log(result);
@@ -207,7 +214,51 @@ router.put('/probabilitytool', (req, res, next) => {
                         // db.close();
                     });
                 } else {
-                    res.json({ msg: "university not found" });
+                    if (req.body.pg_seminars >= 2 && req.body.pg_recommendation == "Good" && req.body.pg_universityreputation == "Good" && req.body.pg_patents >= 2 && req.body.pg_paperpresentation >= 2 && req.body.pg_conference >= 2 && req.body.pg_sports >= 2 && (req.body.pg_extraskills >= 7 && req.body.pg_extraskills <= 9) && req.body.pg_workshops == 2 && req.body.pg_projects == 3 && req.body.pg_internship == "2 Certificates" && req.body.pg_wrkexp == "3 Years" && (req.body.pg_gre >= 305 && req.body.pg_gre <= 314)&& (req.body.pg_gmat >= 600 && req.body.pg_gmat <= 649) && (req.body.pg_sat >= 1250 && req.body.pg_sat <= 1299) && (req.body.pg_toefl >= 95 && req.body.pg_toefl <= 99) && (req.body.pg_ielts >= 6.5 && req.body.pg_ielts <= 9.0)&& (req.body.pg_pte >= 65 && req.body.pg_pte <= 69) && (req.body.pg_Backlogs >= 7 && req.body.pg_Backlogs <= 9)&& (pg_gpa_up >= 75 && pg_gpa_up <= 79)&& (pg_12th_up >= 80 && pg_12th_up <= 84) && (pg_10th_up >= 80 && pg_10th_up <= 84)) {
+                        dbo.collection("universitydatas").find(query).toArray(function (err, gold) {
+                            if (err) throw err;
+                            //console.log(result);
+                            res.json({ msg: "You are eligible to Gold Universities", gold });
+                            // res.json(result);
+                            // db.close();
+                        });
+                    }else{
+                        if (req.body.pg_seminars >= 1 && req.body.pg_recommendation == "Nice" && req.body.pg_universityreputation == "Nice" && req.body.pg_patents >= 1 && req.body.pg_paperpresentation >= 1 && req.body.pg_conference >= 1 && req.body.pg_sports >= 1 && (req.body.pg_extraskills >= 5 && req.body.pg_extraskills <= 6) && req.body.pg_workshops == 2 && req.body.pg_projects == 2 && req.body.pg_internship == "1 Certificate" && req.body.pg_wrkexp == "2 Years" && (req.body.pg_gre >= 295 && req.body.pg_gre <= 304)&& (req.body.pg_gmat >= 550 && req.body.pg_gmat <= 599) && (req.body.pg_sat >= 1200 && req.body.pg_sat <= 1249) && (req.body.pg_toefl >= 90 && req.body.pg_toefl <= 94) && (req.body.pg_ielts >= 6.0 && req.body.pg_ielts <= 9.0)&& (req.body.pg_pte >= 60 && req.body.pg_pte <= 64) && (req.body.pg_Backlogs >= 10 && req.body.pg_Backlogs <= 12)&& (pg_gpa_up >= 65 && pg_gpa_up <= 74)&& (pg_12th_up >= 70 && pg_12th_up <= 79) && (pg_10th_up >= 70 && pg_10th_up <= 79)) {
+                            dbo.collection("universitydatas").find(query).toArray(function (err, silver) {
+                                if (err) throw err;
+                                //console.log(result);
+                                res.json({ msg: "You are eligible to Silver Universities", silver });
+                                // res.json(result);
+                                // db.close();
+                            });
+                        }else{
+                            if (req.body.pg_seminars >= 0 && req.body.pg_recommendation == "Mediocre" && req.body.pg_universityreputation == "Mediocre" && req.body.pg_patents >= 0 && req.body.pg_paperpresentation >= 0 && req.body.pg_conference >= 0 && req.body.pg_sports >= 0 && (req.body.pg_extraskills >= 2 && req.body.pg_extraskills <= 4) && req.body.pg_workshops == 1 && req.body.pg_projects == 1 && req.body.pg_internship == "No Certificates" && req.body.pg_wrkexp == "1 Year" && (req.body.pg_gre >= 285 && req.body.pg_gre <= 294)&& (req.body.pg_gmat >= 500 && req.body.pg_gmat <= 549) && (req.body.pg_sat >= 1150 && req.body.pg_sat <= 1399) && (req.body.pg_toefl >= 80 && req.body.pg_toefl <= 89) && (req.body.pg_ielts >= 5.5 && req.body.pg_ielts <= 9.0)&& (req.body.pg_pte >= 55 && req.body.pg_pte <= 59) && (req.body.pg_Backlogs >= 11 && req.body.pg_Backlogs <= 15)&& (pg_gpa_up >= 60 && pg_gpa_up <= 64)&& (pg_12th_up >= 60 && pg_12th_up <= 69) && (pg_10th_up >= 60 && pg_10th_up <= 69)) 
+                            {
+                                dbo.collection("universitydatas").find(query).toArray(function (err, copper) {
+                                    if (err) throw err;
+                                    //console.log(result);
+                                    res.json({ msg: "You are eligible to Copper Universities", copper });
+                                    // res.json(result);
+                                    // db.close();
+                                });
+
+                            }else{
+                                if (req.body.pg_seminars >= 0 && req.body.pg_recommendation == "Regular" && req.body.pg_universityreputation == "Regular" && req.body.pg_patents >= 0 && req.body.pg_paperpresentation >= 0 && req.body.pg_conference >= 0 && req.body.pg_sports >= 0 && (req.body.pg_extraskills >= 0 && req.body.pg_extraskills <= 1) && req.body.pg_workshops == 0 && req.body.pg_projects == 0 && req.body.pg_internship == "No Certificates" && req.body.pg_wrkexp == "0 Year" && (req.body.pg_gre >= 260 && req.body.pg_gre <= 284)&& (req.body.pg_gmat >= 400 && req.body.pg_gmat <= 500) && (req.body.pg_sat >= 900 && req.body.pg_sat <= 1150) && (req.body.pg_toefl >= 70 && req.body.pg_toefl <= 79) && (req.body.pg_ielts >= 5.0 && req.body.pg_ielts <= 9.0)&& (req.body.pg_pte >= 50 && req.body.pg_pte <= 54) && (req.body.pg_Backlogs >= 14 && req.body.pg_Backlogs <= 18)&& (pg_gpa_up >= 50 && pg_gpa_up <= 60)&& (pg_12th_up >= 40 && pg_12th_up <= 60) && (pg_10th_up >= 40 && pg_10th_up <= 60)) 
+                                {
+                                    dbo.collection("universitydatas").find(query).toArray(function (err, bronze) {
+                                        if (err) throw err;
+                                        //console.log(result);
+                                        res.json({ msg: "You are eligible to Bronze Universities", bronze });
+                                        // res.json(result);
+                                        // db.close();
+                                    });
+                                }
+                                else{
+                                    res.json({ msg: "university not found" });
+                                }
+                            }
+                        }
+                    }
                 }
             }
         }
@@ -226,34 +277,20 @@ router.put('/probability', (req, res, next) => {
         //if (!req.body.pg_10th || !req.body.pg_12th || !req.body.pg_gpa || !req.body.pg_Backlogs || !req.body.pg_gre || !req.body.pg_gmat || !req.body.pg_sat || !req.body.pg_toefl || !req.body.pg_ielts || !req.body.pg_pte || !req.body.pg_wrkexp || !req.body.pg_internship || !req.body.pg_projects || !req.body.pg_workshops || !req.body.pg_extraskills || !req.body.pg_sports || !req.body.pg_conference || !req.body.pg_paperpresentation || !req.body.pg_patents || !req.body.pg_seminars || !req.body.pg_recommendation || !req.body.pg_universityreputation) {
         //   res.json({ success: false, message: 'please fill all requirements' });
         //} else {
+        //var pg_ielts_up = parseFloat(req.body.pg_ielts);
+
+        var x = 10;
+        var pg_10th_up =''+req.body.pg_10th*x+'';
         var query = {
-            pg_10th: req.body.pg_10th,
-            pg_12th: req.body.pg_12th,
-            pg_gpa: req.body.pg_gpa,
-            pg_Backlogs: req.body.pg_Backlogs,
-            pg_gre: req.body.pg_gre,
-            pg_gmat: req.body.pg_gmat,
-            pg_sat: req.body.pg_sat,
-            pg_toefl: req.body.pg_toefl,
-            pg_ielts: req.body.pg_ielts,
-            pg_pte: req.body.pg_pte,
-            pg_wrkexp: req.body.pg_wrkexp,
-            pg_internship: req.body.pg_internship,
-            pg_projects: req.body.pg_projects,
-            pg_workshops: req.body.pg_workshops,
-            pg_extraskills: req.body.pg_extraskills,
-            pg_sports: req.body.pg_sports,
-            pg_conference: req.body.pg_conference,
-            pg_paperpresentation: req.body.pg_paperpresentation,
-            pg_patents: req.body.pg_patents,
-            pg_seminars: req.body.pg_seminars,
-            pg_recommendation: req.body.pg_recommendation,
-            pg_universityreputation: req.body.pg_universityreputation
-
+            pg_10th:pg_10th_up,
+            pg_ielts:pg_ielts_up
         };
-        if(req.body.pg_seminars >= 3 && req.body.pg_recommendation == "Superb" && req.body.pg_universityreputation == "Superb" && req.body.pg_patents >= 3 && req.body.pg_paperpresentation >= 3 && req.body.pg_conference >= 3 && req.body.pg_sports >= 3 && (req.body.pg_extraskills >= 10 && req.body.pg_extraskills <= 11)&&(req.body.pg_workshops == 3 || req.body.pg_workshops == 4) && req.body.pg_projects == 4 && req.body.pg_internship == "3 Certificates" && req.body.pg_wrkexp == "4 Years" &&(req.body.pg_gre >= 315 && req.body.pg_gre <= 324) && (req.body.pg_gmat >= 650 && req.body.pg_gmat <= 679) && (req.body.pg_sat >= 1300 && req.body.pg_sat <= 1399) && (req.body.pg_toefl >= 100 && req.body.pg_toefl <= 109) && (req.body.pg_ielts >= 7 && req.body.pg_ielts <= 9) && (req.body.pg_pte >= 70 && req.body.pg_pte <= 74) && (req.body.pg_Backlogs >= 4 && req.body.pg_Backlogs <= 6)&&(req.body.pg_gpa >= 8 && req.body.pg_gpa <= 8.4)&&(req.body.pg_12th >= 8.5 && req.body.pg_12th <= 8.9) && (req.body.pg_10th >= 8.5 && req.body.pg_10th <= 8.9))
-        {
-
+        console.log(pg_10th_up);
+        console.log(query);
+        console.log(pg_ielts_up);
+        if (pg_10th_up >= 85 && pg_ielts_up >= 7.5) {
+            console.log(pg_10th_up);
+            console.log(query);
             dbo.collection("universitydatas").find(query).toArray(function (err, diamond) {
                 if (err) throw err;
                 //console.log(result);
@@ -265,6 +302,7 @@ router.put('/probability', (req, res, next) => {
             res.json({ msg: "university not found" });
         }
         // }
+
     });
 });
 
